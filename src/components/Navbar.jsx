e3d074cf56a92039933ee9bbf68ec90d44482b5d
clarachence <domitilachence@gmail.com>
2025-04-17 09:32:11 -0300

    ajustes no css do nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,32 +3,28 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <>
-      <nav className={styles.navbar}>
-        <ul className={styles.link_list}>
-          <li>
-            <NavLink to="/" className={({isActive}) => isActive ? `${styles.brand} ${styles.active}` : styles.brand } >
-              <span>Life</span>Dev
+    <nav className={styles.navbar}>
+      <NavLink to="/" className={({ isActive }) => isActive ? `${styles.brand} ${styles.active}` : styles.brand} >
+        <span>Life</span>Dev
+      </NavLink>
+
+      <ul className={styles.link_list}>
+        <li>
+          <NavLink to="/login" className={({ isActive }) => isActive ? `${styles.link} ${styles.active}` : styles.link} >
+            Login
           </NavLink>
-          </li>
+        </li>
 
-          <li>
-            <NavLink to="/login" className={({isActive}) => isActive ? `${styles.link} ${styles.active}` : styles.link } >
-              Login
+        <li>
+          <NavLink to="/register" className={({ isActive }) => isActive ? `${styles.link} ${styles.active}` : styles.link} >
+            Register
           </NavLink>
-          </li>
+        </li>
 
-          <li>
-            <NavLink to="/register" className={({isActive}) => isActive ? `${styles.link} ${styles.active}` : styles.link } >
-              Register
-          </NavLink>
-          </li>
-
-
-          <button className={styles.exit}>Exit</button>
-        </ul>
-      </nav>
-    </>
+        <li>   <button className={styles.exit}>Exit</button>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
